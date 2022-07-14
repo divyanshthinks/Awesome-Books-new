84b9d3a28ec0d6fcca0475ab3d8a863e88b4060f
Divyansh Singh <kalki@Divyanshs-MacBook-Air.local>
2022-07-15 04:19:52 +0530

    fix linter issues

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
 let books = [];
 if (localStorage.books) books = JSON.parse(localStorage.getItem('books'));
 
-const appi = document.getElementById("list-cr");
+const appi = document.getElementById('list-cr');
 
 function create() {
-let card = '';
+  let card = '';
 
-for(let i = 0; i < books.length; i++){
-   const uni = `
+  for (let i = 0; i < books.length; i += 1) {
+    const uni = `
    <ul>
    <li>${books[i].name}</li>
    <li>${books[i].author}</li>
    <li><button data-index = "${i}" class="remove">remove</button></li>
    </ul>
-   `
-   card += uni;
-}
+   `;
+    card += uni;
+  }
 
-appi.innerHTML = card;
+  appi.innerHTML = card;
 
-document.querySelectorAll(".remove").forEach((element) => element.addEventListener('click', (event) =>{
+  document.querySelectorAll('.remove').forEach((element) => element.addEventListener('click', (event) => {
     const getindex = event.currentTarget.dataset.index;
     books.splice(parseInt(getindex, 5), 1);
     create();
     localStorage.setItem('books', JSON.stringify(books));
-}));
+  }));
 }
 
 create();
 
-const newtitle = document.getElementById("title");
-const auth = document.getElementById("author");
+const newtitle = document.getElementById('title');
+const auth = document.getElementById('author');
 
 function addbooks() {
-    const book = {};
-    book.name = newtitle.value;
-    book.author = auth.value;
-    books.push(book);
-    create();
-    localStorage.setItem('books', JSON.stringify(books));
+  const book = {};
+  book.name = newtitle.value;
+  book.author = auth.value;
+  books.push(book);
+  create();
+  localStorage.setItem('books', JSON.stringify(books));
 }
 
-const addbtn = document.getElementById("add");
+const addbtn = document.getElementById('add');
 
 addbtn.addEventListener('click', () => {
-    addbooks();
-})
-
-
-
+  addbooks();
+});
